b8c8435a9df5e734791af7c05ec29ddaaddb150c
Rakib-ullah <rakibullah19730@gmail.com>
2023-10-27 15:13:24 +0600

    dynamic nav ber

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name="homepage"),
-    path('about/', views.About, name="blog_data"),
+    path('', views.homepage, name="blog home"),
+    path('about/', views.About, name="blog about"),
 ]
